d01c7d87f5aa4438af4603ad5792fa5c0efe92a9
Fish <fishagbalo@gmail.com>
2025-04-23 19:50:49 +0800

    Revert into Rails 7

--- rails-store-app/app/controllers/application_controller.rb
+++ rails-store-app/app/controllers/application_controller.rb
@@ -1,23 +1,2 @@
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
-    
-    before_action :authenticate_user
-    
-    private
-  
-    def authenticate_user
-      token = cookies.signed[:jwt]
-      if token
-        begin
-          decoded = JsonWebToken.decode(token)
-          @current_user = User.find(decoded[:user_id])
-        rescue JWT::ExpiredSignature
-          render json: { error: 'Token expired' }, status: :unauthorized
-        rescue JWT::DecodeError, ActiveRecord::RecordNotFound
-          render json: { error: 'Unauthorized' }, status: :unauthorized
-        end
-      else
-        render json: { error: 'No token present' }, status: :unauthorized
-      end
-    end
-  end
+end
